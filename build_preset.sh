39ea4c910e8a6ae1ddcd812cb5d6d74e41a0d9fa
Frank <frank@steamedveggie.com>
2022-10-14 15:16:55 -0400

    Change wording to java 17 for 1.18+

--- build_preset.sh
+++ build_preset.sh
@@ -1,2 +1,2 @@
-echo If java version is not Java 8, this will error out. Please set your JAVA_HOME environment variable with "export JAVA_HOME=/path/to/jdk-8"
+echo If java version is not Java 17, this will error out. Please set your JAVA_HOME environment variable with "export JAVA_HOME=/path/to/jdk-17"
 ./gradlew makePreset
